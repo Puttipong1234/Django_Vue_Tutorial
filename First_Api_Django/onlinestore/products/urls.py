c760c8ec53548459405d9b566c9af341d4275ca8
Puttipong1234 <Puttipong.lims@gmail.com>
2020-01-18 15:55:17 +0700

    first commit

---
+++ First_Api_Django/onlinestore/products/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+
+# from .views import ProductDetailView , ProductListView
+
+# urlpatterns = [
+#      path("",ProductListView.as_view(),name = "product-list"),
+#      path("products/<int:pk>/", ProductDetailView.as_view(),name="product-detail")
+# ]
+
+### end first lesson
+
+from .views import product_list , product_detail
+
+urlpatterns = [
+    path("products/",product_list,name="product-list"),
+    path("products/<int:pk>/",product_detail,name="product-detail")
+]
