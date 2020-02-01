f1455779163ca2c39f59885efba34186ce214e38
Puttipong1234 <Puttipong.lims@gmail.com>
2020-02-01 12:41:06 +0700

    add

---
+++ profilesapi/profiles/signals.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from profiles.models import Profile
+## send signals when post save
+
+@receiver(post_save,sender=User)
+def create_profile(sender,instance,created,**kwargs):
+    print("Created: ", created)
+    if created:
+        Profile.objects.create(user=instance)
