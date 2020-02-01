e3274bfcccbf8474e71e6d71b98c5a5d5288b25e
Puttipong1234 <Puttipong.lims@gmail.com>
2020-02-01 14:00:28 +0700

    search params adding

---
+++ profilesapi/profiles/api/permissions.py
@@ -0,0 +1,14 @@
+from rest_framework import permissions
+
+## owner can be only fix data
+class IsOwnProfileOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user == request.user
+
+class IsOwnOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user_profile == request.user.profile
--- profilesapi/profiles/api/urls.py
+++ profilesapi/profiles/api/urls.py
@@ -1,6 +1,18 @@
-from django.urls import path
-from profiles.api.views import ProfileList
+from django.urls import path , include
+from rest_framework.routers import DefaultRouter
+from profiles.api.views import ProfileViewSet , ProfileStatusViewSet , AvatarUpdateView
+#adding viewset
+# profile_list = ProfileViewSet.as_view({"get":"list"})
+# profile_detail = ProfileViewSet.as_view({"get":"retrieve"})
+
+##use router 
+router = DefaultRouter()
+router.register(r"profiles",ProfileViewSet)
+router.register(r"status",ProfileStatusViewSet , basename="status") #define query set
+
+
 
 urlpatterns = [
-    path("profiles/",ProfileList.as_view(),name="profile-list")
+    path("",include(router.urls)),
+    path("avatar/",AvatarUpdateView.as_view(),name="avatar-update")
 ]
